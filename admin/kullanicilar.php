58ee6cbc96e3f48e4afad089c745c27943541f59
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-06-27 16:51:05 +0300

    Update kullanicilar.php

--- admin/kullanicilar.php
+++ admin/kullanicilar.php
@@ -15,9 +15,14 @@
         echo "<td>" . $row["username"] . "</td>";
         if($row["authorization"] == 3){
             echo "<td>ADMIN (" . $row["authorization"] . ")</td>";
-        } else {
+        } else if( $row["authorization"] == 2) {
             // ! Daha yapilmadi.
-            echo "<td><form action='../determinationsystems/admin/changeauthorization?".$row["id"]."'>" ."<input type='number' placeholder='". $row["authorization"] . "'><button type='submit'>Gonder</button></form></td>";
+            echo "<td><form action='../determinationsystems/admin/changeauthorization?".$row["id"]."'>" ."<input type='number' placeholder='Satis Sorumlusu - ". $row["authorization"] . "'><button type='submit'>Gonder</button></form></td>";
+        } else if($row["authorization"] == 1) {
+            // ! Daha yapilmadi.
+            echo "<td><form action='../determinationsystems/admin/changeauthorization?".$row["id"]."'>" ."<input type='number' placeholder='Tedarikci - ". $row["authorization"] . "'><button type='submit'>Gonder</button></form></td>";
+        } else if($row["authorization"] == 4) {
+            echo "<td><form action='../determinationsystems/admin/changeauthorization?".$row["id"]."'>" ."<input type='number' placeholder='Depocu - ". $row["authorization"] . "'><button type='submit'>Gonder</button></form></td>";
         }
         echo "</tr>";
     }
