c341ce0fff2d23d922f49eaff106bda98136d84d
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-06-27 12:28:10 +0300

    Update satislar.php

--- admin/satislar.php
+++ admin/satislar.php
@@ -4,6 +4,7 @@
 include("../system/connection.php"); 
 
 $onemonthago = date("Y-m-d", strtotime("-1 months"));
+
 $today = date("Y-m-d");
 
 
@@ -11,16 +12,27 @@ $sql = "SELECT * FROM siparisler WHERE tarih BETWEEN '$onemonthago' AND '$today'
 $result = mysqli_query($conn, $sql);
 $sqlmusteriler = "SELECT * FROM musteriler";
 $resultmusteriler = mysqli_query($conn, $sqlmusteriler);
-$rowmusteriler = mysqli_fetch_assoc($resultmusteriler);
 
-while ($row = mysqli_fetch_assoc($result)) {
-    $i = 0;
-    $toplamsayi = $toplamsayi + 1;
-    $fiyat = $rowmusteriler["toplamurunfiyati"] - $rowmusteriler["kapora"];
-    echo $fiyat;
-    $toplamfiyat = $toplamfiyat + $fiyat;
-    $i = $i + 1;
+
+
+while($row = mysqli_fetch_assoc($result)) {
+    $i = $i+1;
 }
-echo "<h1>Toplam Satis:" . $toplamsayi . "</h1>";
+
+while ($rowmusteriler = mysqli_fetch_assoc($resultmusteriler)) {
+    $fiyat = $rowmusteriler["toplamurunfiyati"] - $rowmusteriler["kapora"];
+    $toplamfiyat = $toplamfiyat + $fiyat;
+}
+echo "<h1>Toplam Satis:" . $i . "</h1>";
 echo "<h1>Toplam Kar: ". $toplamfiyat ." </h1>"
 ?>
+
+
+<!-- 
+    Toplam Kar
+    Toplam Satis
+    Toplam musteri
+    En cok satilan
+    En az satilan
+
+ -->
