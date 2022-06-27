c8d004aac49ca287769ca067e1be437546d25e80
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-06-27 12:47:38 +0300

    Update satislar.php

--- admin/satislar.php
+++ admin/satislar.php
@@ -4,12 +4,12 @@
 include("../system/connection.php"); 
 
 $onemonthago = date("Y-m-d", strtotime("-1 months"));
-
 $today = date("Y-m-d");
 
 
 $sql = "SELECT * FROM siparisler WHERE tarih BETWEEN '$onemonthago' AND '$today'";
 $result = mysqli_query($conn, $sql);
+
 $sqlmusteriler = "SELECT * FROM musteriler";
 $resultmusteriler = mysqli_query($conn, $sqlmusteriler);
 
@@ -25,11 +25,9 @@ while ($rowmusteriler = mysqli_fetch_assoc($resultmusteriler)) {
     $fiyat = $rowmusteriler["toplamurunfiyati"] - $rowmusteriler["kapora"];
     $toplamfiyat = $toplamfiyat + $fiyat;
 }
+
 echo "<h1>Toplam Satis:" . $i . "</h1>";
 echo "<h1>Toplam Kar: ". $toplamfiyat ." </h1>";
 echo "<h1>Toplam Musteri Sayisi: ". $musterisayisi ." </h1>";
 ?>
 
-
-<!-- 
- -->
