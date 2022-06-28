58b1ec5799fa7cec3b80fde6029e4cea895ab8c2
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-06-28 16:38:02 +0300

    Update urun-listesi.php

--- pages/urun-listesi.php
+++ pages/urun-listesi.php
@@ -1,49 +1,99 @@
-<link rel="stylesheet" href="style.css">
-
-
-
-<h1>URUNLER</h1>
-  <table cellpadding="0" cellspacing="0" border="0">
-      <thead>
-        <tr>
-          <th>Urun IDsi</th>
-          <th>Urun Isim</th> 
-          <th>Urun Malzemeleri</th> 
-          <th>Urun Renkleri</th> 
-          <th>Envanterdeki Urun Sayisi</th> 
-        </tr>
-      </thead>
-    </table>
-  </div>
-  <div class="tbl-content">
-    <table cellpadding="0" cellspacing="0" border="0">
-      <tbody>
-              <?php
-include ("../system/connection.php");
-$database = mysqli_query($conn, "SELECT * FROM urunler");
-while ($insert = mysqli_fetch_array($database))
-{
-    echo "<tr>";
-    echo "<td><a href= 'urunayarla.php?id=". $insert['urunid']. "'>" . $insert['urunid'] . "</a></td>";
-    echo "<td>" . $insert['urunisim'] . "</td>";
-    echo "<td>" . $insert['urunmalzeme'] . "</td>";
-    echo "<td>" . $insert['urunrenk'] . "</td>";
-    echo "<td>" . $insert['urunEnvanter'] . "</td>";
-    echo "</tr>";
-}
-?>
-      </tbody>
-    </table>
-
-    <h1>Urun Malzeme Degistirme</h1>
-    <form action="../determinationsystems/urunayarla.php?" method="GET">
-        <input type="number" name="urunid" id=""/>
-        <input type="text" placeholder="Orn: kirmizi ayaklik, tahta" name="urunmalzeme" id=""/>
-        <button type="submit">GONDER</button>
-    </form>
-    <h1>Urun Renk Secenekleri Degistirme</h1>
-    <form action="../determinationsystems/urunayarla.php?" method="GET">
-        <input type="number" name="urunid" id=""/>
-        <input type="text" placeholder="Orn: kirmizi, mavi, yesil, mor" name="urunrenk" id=""/>
-        <button type="submit">GONDER</button>
+<link rel="stylesheet" href="style.css">
+
+
+
+
+
+
+
+<h1>URUNLER</h1>
+
+  <table cellpadding="0" cellspacing="0" border="0">
+
+      <thead>
+
+        <tr>
+
+          <th>Urun IDsi</th>
+
+          <th>Urun Isim</th> 
+
+          <th>Urun Malzemeleri</th> 
+
+          <th>Urun Renkleri</th> 
+
+          <th>Envanterdeki Urun Sayisi</th> 
+          <th>Urun QR Olustur</th> 
+
+        </tr>
+
+      </thead>
+
+    </table>
+
+  </div>
+
+  <div class="tbl-content">
+
+    <table cellpadding="0" cellspacing="0" border="0">
+
+      <tbody>
+
+              <?php
+
+include ("../system/connection.php");
+
+$database = mysqli_query($conn, "SELECT * FROM urunler");
+
+while ($insert = mysqli_fetch_array($database))
+
+{
+
+    echo "<tr>";
+
+    echo "<td><a href= 'urunayarla.php?id=". $insert['urunid']. "'>" . $insert['urunid'] . "</a></td>";
+
+    echo "<td>" . $insert['urunisim'] . "</td>";
+
+    echo "<td>" . $insert['urunmalzeme'] . "</td>";
+
+    echo "<td>" . $insert['urunrenk'] . "</td>";
+
+    echo "<td>" . $insert['urunEnvanter'] . "</td>";
+    echo "<td><a href='../determinationsystems/urunicinqrolustur.php?urunid=".$insert["urunid"]."'>QR OLUSTUR</a></td>";
+
+    echo "</tr>";
+
+}
+
+?>
+
+      </tbody>
+
+    </table>
+
+
+
+    <h1>Urun Malzeme Degistirme</h1>
+
+    <form action="../determinationsystems/urunayarla.php?" method="GET">
+
+        <input type="number" name="urunid" id=""/>
+
+        <input type="text" placeholder="Orn: kirmizi ayaklik, tahta" name="urunmalzeme" id=""/>
+
+        <button type="submit">GONDER</button>
+
+    </form>
+
+    <h1>Urun Renk Secenekleri Degistirme</h1>
+
+    <form action="../determinationsystems/urunayarla.php?" method="GET">
+
+        <input type="number" name="urunid" id=""/>
+
+        <input type="text" placeholder="Orn: kirmizi, mavi, yesil, mor" name="urunrenk" id=""/>
+
+        <button type="submit">GONDER</button>
+
     </form>
