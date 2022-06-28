e9bacb19774e0cfed51b9d3308326922c13b5b37
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-06-28 16:54:41 +0300

    Update urunayarla.php

--- pages/urunayarla.php
+++ pages/urunayarla.php
@@ -44,14 +44,24 @@ for ($i=0; $i < count($explodeurunmalzeme) ; $i++) {
 
 
 
+function qrCode($s, $w = 250, $h = 250){
+  $u = 'https://chart.googleapis.com/chart?chs=%dx%d&cht=qr&chl=%s';
+  $url = sprintf($u, $w, $h, $s);
+    return $url;
+}
+
+$urunid = $_GET["id"];
+
+
+$urunlersql = "SELECT * FROM `urunler` WHERE `urunid`='$urunid'";
+$urunlerveri = mysqli_query($conn, $urunlersql);
+$urunlercekveri = mysqli_fetch_array($urunlerveri);
+$urunisim = $urunlercekveri["urunisim"];
+$theword = strval($urunid).strval($urunisim);
+
+  $qr = qrCode($theword, 200, 200); // 200x200
+echo '<img src="'.$qr.'">';
 
 
 
 ?>
-
-<!-- 
-// var a = b
-// var A = b
-// ? bu ne amk
-// * bu sayfada error var
--->
