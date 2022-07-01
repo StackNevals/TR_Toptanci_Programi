5dd4695edf2325e65a80f9a15a86bdd37aa08c16
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-07-01 21:49:58 +0300

    Use Beautifier

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,4 +1,3 @@
-
 <div class="sidebar-menu-inner">
     <header class="logo-env">
         <!-- logo -->
@@ -9,18 +8,20 @@
         </div>
         <!-- logo collapse icon -->
         <div class="sidebar-collapse">
-            <a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+            <a href="#" class="sidebar-collapse-icon">
+                <!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
                 <i class="entypo-menu"></i>
             </a>
         </div>
         <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
         <div class="sidebar-mobile-menu visible-xs">
-            <a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
+            <a href="#" class="with-animation">
+                <!-- add class "with-animation" to support animation -->
                 <i class="entypo-menu"></i>
             </a>
         </div>
     </header>
-                <div class="sidebar-user-info">
+    <div class="sidebar-user-info">
         <div class="sui-normal">
             <a href="#" class="user-link">
                 <img src="assets/images/thumb-1@2x.png" width="55" alt="" class="img-circle" />
@@ -28,7 +29,8 @@
                 <strong>Art Ramadani</strong>
             </a>
         </div>
-        <div class="sui-hover inline-links animate-in"><!-- You can remove "inline-links" class to make links appear vertically, class "animate-in" will make A elements animateable when click on user profile -->
+        <div class="sui-hover inline-links animate-in">
+            <!-- You can remove "inline-links" class to make links appear vertically, class "animate-in" will make A elements animateable when click on user profile -->
             <a href="#">
                 <i class="entypo-pencil"></i>
                 New Page
@@ -41,8 +43,9 @@
                 <i class="entypo-lock"></i>
                 Log Off
             </a>
-            <span class="close-sui-popup">&times;</span><!-- this is mandatory -->				</div>
-    </div>    
+            <span class="close-sui-popup">&times;</span><!-- this is mandatory -->
+        </div>
+    </div>
     <ul id="main-menu" class="main-menu">
         <!-- add class "multiple-expanded" to allow multiple submenus to open -->
         <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
@@ -67,16 +70,14 @@
                         <span class="title">Dashboard 3</span>
                     </a>
                 </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="highlights.html">
-                        <span class="title">What's New</span>
-                        <span class="badge badge-success badge-roundless">v2.0</span>
-                    </a>
-                </li>
             </ul>
-        </ul>
+        </li>
+        <li>
+            <a href="highlights.html">
+                <span class="title">What's New</span>
+                <span class="badge badge-success badge-roundless">v2.0</span>
+            </a>
+        </li>
+    </ul>
+    </ul>
 </div>
-
-
