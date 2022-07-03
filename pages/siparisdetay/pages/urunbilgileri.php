5a2398a9b96948f20eebbe49e587fa9c52bb479d
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-07-03 14:45:35 +0300

    Update urunbilgileri.php

--- pages/siparisdetay/pages/urunbilgileri.php
+++ pages/siparisdetay/pages/urunbilgileri.php
@@ -1,19 +1,19 @@
 <div class="payment">
       <h1>Urun bilgileri</h1>
       <h2>Tedarikci</h2>
-      <input type="text" name="" placeholder=<?php echo $row["tedarikci"];?> id=""><button class="btn">update</button>
+      <input type="text" name="" placeholder=<?php echo $row["tedarikci"];?> id=""/><button class="btn">update</button>
       <h2>Urun ismi</h2>
-      <input type="text" name="" placeholder=<?php echo $row["urunisim"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["urunisim"];?> id=""/>
       <h2>Renk</h2>
-      <input type="text" name="" placeholder=<?php echo $row["urunrenk"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["urunrenk"];?> id=""/>
       <h2>Malzeme ismi</h2>
-      <input type="text" name="" placeholder=<?php echo $row["urunmalzeme1"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["urunmalzeme1"];?> id=""/>
       <h2>2. Malzeme Ismi</h2>
-      <input type="text" name="" placeholder=<?php echo $row["urunmalzeme2"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["urunmalzeme2"];?> id=""/>
       <h2>Siparis Tarihi</h2>
-      <input type="text" name="" placeholder=<?php echo $row["tarih"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["tarih"];?> id=""/>
       <h2>Siparis Notu</h2>
-      <input type="text" name="" placeholder=<?php echo $row["siparisnotu"];?> id="">
+      <input type="text" name="" placeholder=<?php echo $row["siparisnotu"];?> id=""/>
       <h2>Siparis Tedarikci Notu</h2>
-      <input type="text" name="" placeholder=<?php echo $row["tedarikcinotu"];?> id="">
-    </div>
+      <input type="text" name="" placeholder=<?php echo $row["tedarikcinotu"];?> id=""/>
+</div>
