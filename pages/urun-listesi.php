c95f472bd658076a40f8df16228fdd732b7136ab
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-07-03 22:06:52 +0300

    BUG FIX

--- pages/urun-listesi.php
+++ pages/urun-listesi.php
@@ -1,268 +1,99 @@
 <!-- <link rel="stylesheet" href="style.css"> -->
-
-
-
-
-
-
-
-<script>
 <?php
 include("../system/connection.php");
 
-function qrCode($s, $w = 250, $h = 250){
 
+function qryaz($urunid,$urunisim,$w = 200,$h = 200) {
   $u = 'https://chart.googleapis.com/chart?chs=%dx%d&cht=qr&chl=%s';
-
+  
+  
+  $s = strval($urunid).strval($urunisim);
+  
   $url = sprintf($u, $w, $h, $s);
-
-    return $url;
-
+  
+  return $url;
+  
 }
-
-
-
-$urunid = $_GET["id"];
-
-
-
-
-$urunlersql = "SELECT * FROM `urunler` WHERE `urunid`='$urunid'";
-
-$urunlerveri = mysqli_query($conn, $urunlersql);
-
-$urunlercekveri = mysqli_fetch_array($urunlerveri);
-
-$urunisim = $urunlercekveri["urunisim"];
-
-$theword = strval($urunid).strval($urunisim);
-
-
-
-  $qr = qrCode($theword, 200, 200); // 200x200
-  /*
-  function qryaz()
-  {
-    var URL = '.$qr.'; 
-    var W = window.open(URL); 
-    W.window.print();
-  }
-*/
-
-
-
-
-
-
 
 ?>
-function qryazdir(){
-  var URL = '<?php echo $qr; ?>';
-  var W = window.open(URL);
-  W.window.print();
-}
-</script>
-
 
 
 
 
 
 <h1>URUNLER</h1>
+<h1>
+  <?php echo $urunisim; ?>
+</h1>
 
 
 
+<table cellpadding="0" cellspacing="0" border="0">
+  <thead>
+    <tr>
+      <th>Urun IDsi</th>
+      <th>Urun Isim</th>
+      <th>Urun Malzemeleri</th>
+      <th>Urun Renkleri</th>
+      <th>Envanterdeki Urun Sayisi</th>
+      <th>Qr Yazdır</th>
+    </tr>
+  </thead>
+</table>
+</div>
+<div class="tbl-content">
   <table cellpadding="0" cellspacing="0" border="0">
-
-
-
-      <thead>
-
-
-
-        <tr>
-
-
-
-          <th>Urun IDsi</th>
-
-
-
-          <th>Urun Isim</th> 
-
-
-
-          <th>Urun Malzemeleri</th> 
-
-
-
-          <th>Urun Renkleri</th> 
-
-
-
-          <th>Envanterdeki Urun Sayisi</th> 
-
-
-            <th>Qr Yazdır</th>
-
-        </tr>
-
-
-
-      </thead>
-
-
-
-    </table>
-
-
-
-  </div>
-
-
-
-  <div class="tbl-content">
-
-
-
-    <table cellpadding="0" cellspacing="0" border="0">
-
-
-
-      <tbody>
-
-
-
-              <?php
-
-
-
-include ("../system/connection.php");
-
-
-
+    <tbody>
+      <?php
 $database = mysqli_query($conn, "SELECT * FROM urunler");
 
-
-
-while ($insert = mysqli_fetch_array($database))
-
-
-
-{
-
-
-
+while ($insert = mysqli_fetch_array($database)){
     echo "<tr>";
-
-
-
     echo "<td><a href= 'urunayarla.php?id=". $insert['urunid']. "'>" . $insert['urunid'] . "</a></td>";
-
-
-
     echo "<td>" . $insert['urunisim'] . "</td>";
-
-
-
     echo "<td>" . $insert['urunmalzeme'] . "</td>";
-
-
-
     echo "<td>" . $insert['urunrenk'] . "</td>";
+    echo "<td>" . $insert['urunEnvanter'] . "</td>";    
+    $urunid = $insert["urunid"];
+    $urunisim = $insert["urunisim"];  
 
+    $qr = qryaz($urunid,$urunisim, 200, 200); // 200x200
+    echo "<script>function qryazdir".$urunid."(){
+      var URL = '".$qr."';
+      var W = window.open(URL);
+      W.window.print();}</script>";
 
+      echo "<td><button onClick='qryazdir".$urunid."()'>Qr Yazdır</button></td>";
+      echo "</tr>";
+    }?>
 
-    echo "<td>" . $insert['urunEnvanter'] . "</td>";
 
 
-    echo "<td><button onClick='qryazdir()'>Qr Yazdır</button></td>";
- 
- echo "</tr>";
+    </tbody>
 
 
 
-}
+  </table>
 
 
 
-?>
 
 
 
-      </tbody>
 
+  <h1>Urun Malzeme Degistirme</h1>
 
 
-    </table>
 
+  <form action="../determinationsystems/urunayarla.php?" method="GET">
 
 
 
+    <input type="number" name="urunid" id="" />
 
 
 
-    <h1>Urun Malzeme Degistirme</h1>
-
-
-
-    <form action="../determinationsystems/urunayarla.php?" method="GET">
-
-
-
-        <input type="number" name="urunid" id=""/>
-
-
-
-        <input type="text" placeholder="Orn: kirmizi ayaklik, tahta" name="urunmalzeme" id=""/>
-
-
-
-        <button type="submit">GONDER</button>
-
-
-
-    </form>
-
-
-
-    <h1>Urun Renk Secenekleri Degistirme</h1>
-
-
-
-    <form action="../determinationsystems/urunayarla.php?" method="GET">
-
-
-
-        <input type="number" name="urunid" id=""/>
-
-
-
-        <input type="text" placeholder="Orn: kirmizi, mavi, yesil, mor" name="urunrenk" id=""/>
-
-
-
-        <button type="submit">GONDER</button>
-
-
-
-    </form>
-
-
-
-    <h1>Urun Envanter Sayisi Degistirme</h1>
-
-
-
-<form action="../determinationsystems/urunayarla.php?" method="GET">
-
-
-
-    <input type="number" name="urunid" id=""/>
-
-
-
-    <input type="number" placeholder="Sayi Giriniz" name="urunsayisi" id=""/>
+    <input type="text" placeholder="Orn: kirmizi ayaklik, tahta" name="urunmalzeme" id="" />
 
 
 
@@ -270,4 +101,52 @@ while ($insert = mysqli_fetch_array($database))
 
 
 
-</form>
+  </form>
+
+
+
+  <h1>Urun Renk Secenekleri Degistirme</h1>
+
+
+
+  <form action="../determinationsystems/urunayarla.php?" method="GET">
+
+
+
+    <input type="number" name="urunid" id="" />
+
+
+
+    <input type="text" placeholder="Orn: kirmizi, mavi, yesil, mor" name="urunrenk" id="" />
+
+
+
+    <button type="submit">GONDER</button>
+
+
+
+  </form>
+
+
+
+  <h1>Urun Envanter Sayisi Degistirme</h1>
+
+
+
+  <form action="../determinationsystems/urunayarla.php?" method="GET">
+
+
+
+    <input type="number" name="urunid" id="" />
+
+
+
+    <input type="number" placeholder="Sayi Giriniz" name="urunsayisi" id="" />
+
+
+
+    <button type="submit">GONDER</button>
+
+
+
+  </form>
