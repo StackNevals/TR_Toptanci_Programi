9f3e65110d161c700e003b9dd779e55b7b45d2a3
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-07-03 15:09:14 +0300

    Update fiyatbilgileri.php

--- pages/siparisdetay/pages/fiyatbilgileri.php
+++ pages/siparisdetay/pages/fiyatbilgileri.php
@@ -1,12 +1,10 @@
 
     <div class="subscription">
       <h1>Fiyat Hakkinda</h1>
-      <h2>Kapora</h2>
-      <input type="text" name="" placeholder="Julie Park" id=""><button class="btn">update</button>
       <h2>Urun Fiyati</h2>
-      <input type="email" name="" placeholder="email" id="">
-      <h2>Malzeme ismi</h2>
-      <input type="tel" name="" placeholder="Yedek Telefon" id="">
-      <h2>2. Malzeme Ismi</h2>
-      <input type="tel" name="" placeholder="Sehir" id="">
+      <input type="text" name="" placeholder=<?php echo $row["urunfiyat"]?> id="">
+      <h2>Kapora</h2>
+      <input type="text" name="" placeholder=<?php echo $row["kapora"]?> id=""><button class="btn">update</button>
+      <h2>Kalan Borc</h2>
+      <input type="text" name="" placeholder=<?php echo $borc = $row["urunfiyat"]-$row["kapora"]?> id="">
     </div>
