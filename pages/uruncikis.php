f85822e31a523523c981ab448cbcb2eccdba7422
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2022-07-04 14:29:10 +0300

    Create necessary files

---
+++ pages/uruncikis.php
@@ -0,0 +1,3 @@
+<form action="../determinationsystems/urunqrcikisi.php" method="POST">
+    <input type="file" name="qrkodu" id="">
+</form>
---
+++ pages/urungiris.php
@@ -0,0 +1,3 @@
+<form action="../determinationsystems/urunqrgirisi.php" method="POST">
+    <input type="file" name="qrkodu" id="">
+</form>
